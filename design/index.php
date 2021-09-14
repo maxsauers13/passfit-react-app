d683067823d32bc25108cdc1fad7851cad47a14c
maxsauers <msauers2@illinois.edu>
2021-09-13 21:04:27 -0500

    pie chart

--- design/index.php
+++ design/index.php
@@ -13,13 +13,22 @@
 
 <h1 class="title">Passfit</h1>
 
+<img class="piechart" src="img/piechart.png" usemap="#piechartmain"></img>
+
+<map name="piechartmain">
+  <area shape="rect" coords="50,100,175,200" alt="Physical" href="physical.php">
+  <area shape="rect" coords="250,100,375,200" alt="Cognitive" href="cognitive.php">
+  <area shape="rect" coords="125,250,300,350" alt="Psychological" href="psychological.php">
+</map>
+
+<!--
 <a class="form-link" href="physical.php">Pysical</a>
 <a class="form-link" href="cognitive.php">Cognitive</a>
 
 <div class="form-link-divider"></div>
 <a class="form-link" href="psychological.php">Psychological</a>
 <div class="form-link-divider"></div>
-
+-->
 <div class="row">
   <div class="form-link-divider"></div>
 </div>
---
+++ design/login.php
@@ -0,0 +1,29 @@
+<!-- HEADER -->
+<?php
+  include_once 'header.php'
+?>
+
+<html>
+<head>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+<h3 class="title-form">Login</h3>
+
+<h5 class="title-question">Username</h5>
+<input class="text-input" type="text" placeholder="Username"><br>
+
+<h5 class="title-question">Password</h5>
+<input class="text-input" type="text" placeholder="Password"><br>
+
+<button class="submit-button" type="submit">Login</button>
+
+</body>
+</html>
+
+<!-- FOOTER -->
+<?php
+  include_once 'footer.php'
+?>
